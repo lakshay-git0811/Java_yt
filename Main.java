1b85329e3053b87b025198314024a269081c1102
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-10 15:38:41 +0530

    adding some classes

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // output
+        System.out.println("Hello World with java");
+        System.out.print("Hello world with java\n");
+        System.out.println("Hello world with java\nfrom\napni kaksha");
+
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        // variables
+        // String name = "Tony Stark";
+        // int age = 48;
+        // double price = 25.25;
+        // int a = 25;
+        // int b = 10;
+
+        // b = 20;
+        // name = "ironman";
+
+        int a = 10;
+        int b = 25;
+        // int sum = a + b;
+        // System.out.println(sum);
+
+        int mul = a * b;
+        System.out.println(mul);
+    }
+}
