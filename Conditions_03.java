1eb67d6415b1846152d3529b8522e86ce1bca7f2
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-11 11:23:15 +0530

    Create Conditions_03.java

---
+++ Conditions_03.java
@@ -0,0 +1,25 @@
+// switch case
+
+import java.util.Scanner;
+
+public class Conditions_03 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        switch (button) {
+            case 1:
+                System.out.println("hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+            default:
+                System.out.println("Invalid Button");
+        }
+    }
+}
