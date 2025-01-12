15b3109e29587ca7a36d4c046a6a52f10934030a
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-12 22:48:31 +0530

    Update BasicPatterns.java

--- BasicPatterns.java
+++ BasicPatterns.java
@@ -13,15 +13,26 @@ public class BasicPatterns {
         // }
 
         // hollow rectangle
+
+        // int n = 4;
+        // int m = 5;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= m; j++) {
+        // if (i == 1 || i == n || j == 1 || j == m) {
+        // System.out.print("*");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // half pyramid
+
         int n = 4;
-        int m = 5;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (i == 1 || i == n || j == 1 || j == m) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
