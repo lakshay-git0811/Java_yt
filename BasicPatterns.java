deb5c415946d964468c2a7968c98fd0f960db88f
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-12 22:32:24 +0530

    Update BasicPatterns.java

--- BasicPatterns.java
+++ BasicPatterns.java
@@ -1,12 +1,27 @@
 public class BasicPatterns {
     public static void main(String[] args) {
-        // solid rectangle
-        int r = 4;
-        int c = 5;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                System.out.print(("*"));
+        // solid rectangle
+
+        // int r = 4;
+        // int c = 5;
+        // for (int i = 0; i < r; i++) {
+        // for (int j = 0; j < c; j++) {
+        // System.out.print(("*"));
+        // }
+        // System.out.println();
+        // }
+
+        // hollow rectangle
+        int n = 4;
+        int m = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
