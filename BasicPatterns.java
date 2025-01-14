13fb83c37909d3500472b1b80ff254ee1b607359
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-14 17:18:27 +0530

    Update BasicPatterns.java

--- BasicPatterns.java
+++ BasicPatterns.java
@@ -72,12 +72,23 @@ public class BasicPatterns {
         // }
 
         // half pyramid with numbers
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // inverted half pyramid with numbers
+        // int n = 5;
+        // for (int i = n; i > 0; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        
     }
 }
