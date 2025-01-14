55356b36ee61caaa113b6ba1e61341d59624a305
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-14 17:46:35 +0530

    Update BasicPatterns.java

--- BasicPatterns.java
+++ BasicPatterns.java
@@ -90,12 +90,25 @@ public class BasicPatterns {
         // }
 
         // floyd's triangle
+        // int n = 5;
+        // int result = 1;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(result + " ");
+        // result++;
+        // }
+        // System.out.println();
+        // }
+
+        // 0-1 Triangle
         int n = 5;
-        int result = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(result + " ");
-                result++;
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
