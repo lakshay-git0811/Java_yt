b41f130c0496852b42921cf8f68f57c166abfac1
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-15 19:45:23 +0530

    Update AdvancedPatterns.java

--- AdvancedPatterns.java
+++ AdvancedPatterns.java
@@ -64,19 +64,52 @@ public class AdvancedPatterns {
         // }
 
         // palindrome pattern
-        int n = 5;
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // // spaces
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // // first part numbers
+        // for (int j = i; j >= 1; j--) {
+        // System.out.print(j);
+        // }
+        // // second part numbers
+        // for (int j = 2; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // Diamond pattern
+        int n = 4;
         for (int i = 1; i <= n; i++) {
             // spaces
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
-            // first part numbers
+            // first part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            // second part
+            for (int j = i; j > 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            // spaces
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print("  ");
+            }
+            // first part
             for (int j = i; j >= 1; j--) {
-                System.out.print(j);
+                System.out.print("* ");
             }
-            // second part numbers
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
+            // second part
+            for (int j = 1; j < i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
