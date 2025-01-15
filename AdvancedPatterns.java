8fbc962df173b1ff212504b2c7883460bf6d82a8
lakshay-git0811 <lakshaysasharma@gmail.com>
2025-01-15 16:35:55 +0530

    Update AdvancedPatterns.java

--- AdvancedPatterns.java
+++ AdvancedPatterns.java
@@ -35,15 +35,30 @@ public class AdvancedPatterns {
 
         // Solid Rhombus
 
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // // spaces
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // // stars
+        // for (int j = 1; j <= n; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // number pyramid
         int n = 5;
+
         for (int i = 1; i <= n; i++) {
             // spaces
             for (int j = 1; j <= n - i; j++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
-            // stars
-            for (int j = 1; j <= n; j++) {
-                System.out.print("* ");
+            // numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
